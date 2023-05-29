9af0b0071e264af58797f62a7d11d1a12423e3a1
Rinkit Adhana <therinkit@gmail.com>
2023-05-29 18:04:35 +0530

    button added

---
+++ assets/js/rain-mute.js
@@ -0,0 +1,13 @@
+
+var audio = document.getElementById("myAudio");
+var muteButton = document.getElementById("muteButton");
+
+function toggleMute() {
+  if (audio.muted) {
+    audio.muted = false;
+    muteButton.classList.remove("muted");
+  } else {
+    audio.muted = true;
+    muteButton.classList.add("muted");
+  }
+}
